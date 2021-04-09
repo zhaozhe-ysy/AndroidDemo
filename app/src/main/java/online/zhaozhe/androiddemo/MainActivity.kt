102d81bad7f98fcf5e80e7f39309fc73cee3d44e
zhaozhe-ysy <zhaozhe076@gmail.com>
2021-04-09 13:45:43 +0800

    [ADD] modify main activity

--- app/src/main/java/online/zhaozhe/androiddemo/MainActivity.kt
+++ app/src/main/java/online/zhaozhe/androiddemo/MainActivity.kt
@@ -8,6 +8,10 @@ import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import online.zhaozhe.androiddemo.entity.User
 import online.zhaozhe.androiddemo.widget.CodeView
 import online.zhaozhe.core.utils.CacheUtils
@@ -17,65 +21,26 @@ import online.zhaozhe.lesson.LessonActivity
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private val usernameKey = "username"
-    private val passwordKey = "password"
 
-    private lateinit var et_username: EditText
-    private lateinit var et_password: EditText
-    private lateinit var et_code: EditText
-    private lateinit var btn_login: Button
-    private lateinit var img_code: CodeView
+
+    private val mainAdapter = MainAdapter(this);
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("zhaozhe", "11111111111")
-        et_username = findViewById(R.id.et_username)
-        et_password = findViewById(R.id.et_password)
-        et_code = findViewById(R.id.et_code)
-        btn_login = findViewById(R.id.btn_login)
-        img_code = findViewById(R.id.code_view)
 
-        et_username.setText(CacheUtils.get(usernameKey))
-        et_password.setText(CacheUtils.get(passwordKey))
+        findViewById<RecyclerView>(R.id.main_recycler_view).run {
+            this.layoutManager = LinearLayoutManager(this@MainActivity)
+            this.adapter = mainAdapter
+            this.addItemDecoration(DividerItemDecoration(this@MainActivity, LinearLayout.VERTICAL))
+        }
 
-        btn_login.setOnClickListener(this)
-        img_code.setOnClickListener(this)
+
     }
 
 
     override fun onClick(v: View?) {
-        if (v is CodeView){
-            v.updateCode()
-        } else if (v is Button){
-            login()
-        }
+
     }
-
-    private fun login(){
-        val username = et_username.text.toString()
-        val password = et_password.text.toString()
-        val code = et_code.text.toString()
-
-
-        fun verify(user: User): Boolean{
-            if (user.username?.length ?: 0 < 4){
-                Utils.toast("用户名不合法")
-                return false
-            }
-            if (user.password?.length ?: 0 < 4){
-                Utils.toast("密码不合法")
-                return false
-            }
-            return true
-        }
-
-        if (verify(User(username,password,code))){
-            CacheUtils.save(usernameKey,username)
-            CacheUtils.save(passwordKey,password)
-            startActivity(Intent(this, LessonActivity::class.java))
-        }
-    }
-
 
 }
---
+++ app/src/main/java/online/zhaozhe/androiddemo/MainAdapter.kt
@@ -0,0 +1,43 @@
+package online.zhaozhe.androiddemo
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import online.zhaozhe.androiddemo.activity.KotlinActivity
+import online.zhaozhe.core.BaseViewHolder
+
+class MainAdapter(private val context: Context) : RecyclerView.Adapter<MainAdapter.MainViewHolder>(){
+
+    private val data = arrayOf("kotlin","retrofit")
+
+    private val activityArray = arrayOf(KotlinActivity::class.java)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_main,parent,false)
+        return MainViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.textView.text = data[position]
+        holder.textView.setOnClickListener {
+            context.startActivity(Intent(context,activityArray[position]))
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+
+    class MainViewHolder internal constructor(itemView: View) : BaseViewHolder(itemView){
+
+        val textView: TextView = itemView.findViewById(R.id.item_text)
+
+    }
+
+
+}
